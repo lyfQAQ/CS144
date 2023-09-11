be9646f97939218b23e5658e144cc3253c817da7
lyfQAQ <0219lyf@gmail.com>
2023-09-11 08:30:49 +0000

    lab0 byte_team

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -4,74 +4,89 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), buffer{} {}
 
 void Writer::push( string data )
 {
   // Your code here.
-  (void)data;
+  size_t n = min(data.size(), capacity_ - buffer.size());
+  
+  for(size_t i = 0; i < n; i++) buffer.push_back(data[i]);
+  num_bytes_pushed_ += n;
+
+  // (void)data;
 }
 
 void Writer::close()
 {
   // Your code here.
+  closed_ = true;
 }
 
 void Writer::set_error()
 {
   // Your code here.
+  error_ = true;
 }
 
 bool Writer::is_closed() const
 {
   // Your code here.
-  return {};
+  return closed_;
 }
 
 uint64_t Writer::available_capacity() const
 {
   // Your code here.
-  return {};
+  return capacity_ - buffer.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
   // Your code here.
-  return {};
+  return num_bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
   // Your code here.
-  return {};
+  return buffer.empty() ? string_view() : string_view(&buffer.front(), 1);
 }
 
 bool Reader::is_finished() const
 {
   // Your code here.
-  return {};
+  return closed_ && this->buffer.empty();
 }
 
 bool Reader::has_error() const
 {
   // Your code here.
-  return {};
+  return error_;
 }
 
 void Reader::pop( uint64_t len )
 {
   // Your code here.
-  (void)len;
+  if(len > buffer.size()) {
+    return;
+  }
+  num_bytes_poped_ += len;
+  while(len--) {
+    buffer.pop_front();
+  }
+  // buffer.erase(buffer.begin(), buffer.begin() + len);
+  
 }
 
 uint64_t Reader::bytes_buffered() const
 {
   // Your code here.
-  return {};
+  return buffer.size();
 }
 
 uint64_t Reader::bytes_popped() const
 {
   // Your code here.
-  return {};
+  return num_bytes_poped_;
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -13,7 +13,11 @@ class ByteStream
 protected:
   uint64_t capacity_;
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
-
+  std::deque<char> buffer;
+  bool closed_ = false;
+  bool error_ = false;
+  uint64_t num_bytes_pushed_ = 0;
+  uint64_t num_bytes_poped_ = 0;
 public:
   explicit ByteStream( uint64_t capacity );
 
